981851456cece246699299d70dc88be3bd5ca66c
Fabrício Pinto Ferreira <pferreirafabricio@gmail.com>
2022-05-09 14:45:06 -0300

    feat: add script to create a system task on windows

---
+++ create-task.ps1
@@ -0,0 +1,78 @@
+# $scriptToExecutePath = "C:\\backup-mongo.ps1";
+# $taskName = "MongoDB Backup - <databaseName>"
+# $taskDescription = "Make a dump of <databaseName> database"
+
+$ErrorActionPreference = 'Stop'
+
+$pathScriptToExecute = ""
+$taskName = ""
+$taskDescription = ""
+
+function Show-ErrorMessage([string] $message) {
+  Write-Host -Message $message `
+    -ForegroundColor Red `
+    -BackgroundColor White
+}
+
+function Get-TaskInfos {
+  $script:pathScriptToExecute = Read-Host "Please enter the path to the PowerShell script"
+  $script:taskName = Read-Host "Please enter the name of the task"
+  $script:taskDescription = Read-Host "Please enter the description of the task (optional)"
+
+  Check-Params
+}
+
+function Check-Params {
+  if ([String]::IsNullOrEmpty($pathScriptToExecute) -or -not ($pathScriptToExecute | Test-Path)  ) {
+    Show-ErrorMessage "The script path doesn't exists"
+    $script:pathScriptToExecute = Read-Host "Please enter the path to the PowerShell script"
+    Check-Params
+    return
+  }
+
+  if ([String]::IsNullOrEmpty($taskName)) {
+    Show-ErrorMessage "The name of the task can not be empty"
+    $script:taskName = Read-Host "Please enter the username"
+    Check-Params
+    return;
+  }
+}
+
+Get-TaskInfos
+
+$taskExists = Get-ScheduledTask | Where-Object { $_.TaskName -like $taskName }
+
+if ($taskExists) {
+  Show-ErrorMessage "An taks with the name $taskName already exists!"
+  exit
+}
+
+if ([String]::IsNullOrEmpty($taskDescription)) {
+  $taskDescription = "MongoDB Backup"
+}
+
+$action = New-ScheduledTaskAction `
+  -Execute 'C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe' `
+  -Argument "-NoProfile -NoLogo -NonInteractive -ExecutionPolicy Bypass -File $pathScriptToExecute"
+
+$trigger = New-ScheduledTaskTrigger -Daily -At 1am
+
+$taskSettings = New-ScheduledTaskSettingsSet -Compatibility Win8
+
+Register-ScheduledTask `
+  -TaskName $taskName `
+  -Action $action `
+  -Trigger $trigger `
+  -Settings $taskSettings `
+  -Description $taskDescription
+
+$runTask = Read-Host "Run task immediatly? [y/n] (y) "
+
+$noOptions = @('n', 'no')
+
+if ($runTask -in $noOptions) {
+  return
+}
+
+# OBS: If you need to run the task manually for testing use:
+Start-ScheduledTask -TaskName $taskName
