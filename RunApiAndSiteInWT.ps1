09aa8333291dfd84a26049ef85772223f8ceaa97
Fabrício Pinto Ferreira <pferreirafabricio@gmail.com>
2023-05-06 18:51:36 -0300

    feat: add script to run an api and site in windows terminal

---
+++ RunApiAndSiteInWT.ps1
@@ -0,0 +1,30 @@
+<#
+.DESCRIPTION
+    Run an API and a web site inside windows terminal with automtic split
+#>
+
+$ErrorActionPreference = 'Stop';
+
+[string]$executionString = ""
+$runApi = 'new-tab --title "API" -d "./Example/Api" -p "Windows PowerShell" powershell -noexit "dotnet watch run"'
+$runSite = 'split-pane --title "Site" --horizontal -d "./Example/Site" -p "Windows PowerShell" powershell -noexit "npm run serve"'
+
+$executionString = "$runApi; $runSite"
+
+# Verify if Windows Terminal is installed
+$exists = (Get-Command wt -ErrorAction SilentlyContinue).Path
+
+[string]$wtError = "Sometimes the Windows Terminal is installed but the wt.exe is not accessible, so follow this steps: https://stackoverflow.com/a/67888339/12542704`n";
+
+if (-not $exists) {
+    Write-Host "To run this script Windows Terminal must be installed. Download here: https://www.microsoft.com/en-us/p/windows-terminal/9n0dx20hk701"
+    Write-Host $wtError;
+    return;
+}
+
+try {
+    Start-Process wt $executionString -ErrorAction SilentlyContinue
+}
+catch {
+    Write-Error $wtError 
+}
