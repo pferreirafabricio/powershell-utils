533112f02f4e4fd47715fa9fe9b97325d7782e43
Fabrício Pinto Ferreira <pferreirafabricio@gmail.com>
2023-05-07 11:36:25 -0300

    fix: correct error in windows powershell

--- RenameFilesInSequence.ps1
+++ RenameFilesInSequence.ps1
@@ -9,7 +9,7 @@
 Get-ChildItem -Path $FolderPath -Filter $Filters `
 | Sort-Object -Property CreationTime `
 | ForEach-Object { $index = 1 } {
-  $currentPath = $_.ToString()
+  $currentPath = $_.FullName.ToString()
 
   $newName = ([string]$index).PadLeft(3, '0')
   Rename-Item -Path $currentPath -NewName "$newName.$FileExtension"
