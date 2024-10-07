a43ee5cb91a77db9f467c85352595c5b6172fae8
Fabrício Pinto Ferreira <pferreirafabricio@gmail.com>
2024-10-07 13:43:36 -0300

    feat: add CheckIfIsAdmin.ps1

---
+++ CheckIfIsAdmin.ps1
@@ -0,0 +1,8 @@
+[bool]$isAdmin = $false
+try {
+    $identity = [System.Security.Principal.WindowsIdentity]::GetCurrent()
+    $principal = New-Object System.Security.Principal.WindowsPrincipal($identity)
+    $isAdmin = $principal.IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator)
+} catch {
+    $isAdmin = $false
+}
